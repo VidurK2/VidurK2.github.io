d11cd490974931177ef8f95225f87b26a1e80534
Vidur Kaushik <54384846+VidurK2@users.noreply.github.com>
2023-03-27 16:13:39 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+<body>
+	<div class="container">
+		<h1>Sign Up</h1>
+		<form method="post" action="process.php">
+			<label for="name">Name:</label>
+			<input type="text" id="name" name="name" required>
+
+			<label for="email">Email:</label>
+			<input type="email" id="email" name="email" required>
+
+			<label for="password">Password:</label>
+			<input type="password" id="password" name="password" required>
+
+			<div class="g-recaptcha" data-sitekey="YOUR_SITE_KEY"></div>
+
+			<input type="submit" value="Sign Up">
+		</form>
+	</div>
+</body>
+</html>
